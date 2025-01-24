bc7c3104b29154baec98288ab059cfda23e2c7f7
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2025-01-24 21:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
+function Person(name,age){
+this.name=name
+this.age=age
+
+}
+Person.prototype.greet=function(){
+console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+
+}
+function Employee(name,age,jobTitle){
+Person.call(this,name,age)
+this.jobTitle=jobTitle
+
 }
 
-Person.prototype.greet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+
+Employee.prototype=Object.create(Person.prototype)
+Employee.prototype.constructor=Employee
+
+Employee.prototype.jobGreet=function(){
+console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+
 }
 
-function Employee(name, age, jobTitle) {
-    Person.call(this, name, age);
-    this.jobTitle = jobTitle;
-}
-
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
-
-Employee.prototype.jobGreet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-}
-
-const person = new Person('Alice', 25);
-person.greet();
-
-const employee = new Employee('Bob', 30, 'Manager');
-employee.jobGreet();
-// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
